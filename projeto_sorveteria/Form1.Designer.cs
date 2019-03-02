a7e1d243ae96990dacc9aeb3c860ca2c98464464
Peckins <messias.morais@etec.sp.gov.br>
2019-03-02 17:49:06 -0300

    Diretórios e arquivos necessários para o funcionamento do programa.

---
+++ projeto_sorveteria/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace projeto_sorveteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbxTipo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxCreme = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxFlocos = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxCobertura = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxFrescuras = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtDinheiro = new System.Windows.Forms.TextBox();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Old English Text MT", 21.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(164, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sorveteria magteina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(6, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sabor:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.txtQuantidade);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbxTipo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(91, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 60);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Picolé Quantidade";
+            // 
+            // cbxTipo
+            // 
+            this.cbxTipo.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cbxTipo.FormattingEnabled = true;
+            this.cbxTipo.Items.AddRange(new object[] {
+            "Morango",
+            "Chocolate",
+            "Maçã",
+            "Cenoura",
+            "Sabão",
+            "Pneu usado",
+            "Maracujá",
+            "Flocos",
+            "Aquele bagui azul lá"});
+            this.cbxTipo.Location = new System.Drawing.Point(68, 28);
+            this.cbxTipo.Name = "cbxTipo";
+            this.cbxTipo.Size = new System.Drawing.Size(121, 21);
+            this.cbxTipo.TabIndex = 2;
+            this.cbxTipo.SelectedIndexChanged += new System.EventHandler(this.cbxTipo_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(237, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Quantidade:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtQuantidade.Location = new System.Drawing.Point(329, 28);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(72, 20);
+            this.txtQuantidade.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.cbxFrescuras);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.cbxCobertura);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.cbxFlocos);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.cbxCreme);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(91, 138);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(424, 116);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bolas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Creme:";
+            // 
+            // cbxCreme
+            // 
+            this.cbxCreme.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cbxCreme.FormattingEnabled = true;
+            this.cbxCreme.Items.AddRange(new object[] {
+            "Creme",
+            "Emerc",
+            "Caramellow",
+            "Wollemarac"});
+            this.cbxCreme.Location = new System.Drawing.Point(63, 24);
+            this.cbxCreme.Name = "cbxCreme";
+            this.cbxCreme.Size = new System.Drawing.Size(121, 21);
+            this.cbxCreme.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(223, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Flocos:";
+            // 
+            // cbxFlocos
+            // 
+            this.cbxFlocos.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cbxFlocos.FormattingEnabled = true;
+            this.cbxFlocos.Items.AddRange(new object[] {
+            "Cereal",
+            "Granola",
+            "Raspas de chifre de unicórnio",
+            "Sucrilhos",
+            "Chumbinho"});
+            this.cbxFlocos.Location = new System.Drawing.Point(281, 24);
+            this.cbxFlocos.Name = "cbxFlocos";
+            this.cbxFlocos.Size = new System.Drawing.Size(121, 21);
+            this.cbxFlocos.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Cobertura:";
+            // 
+            // cbxCobertura
+            // 
+            this.cbxCobertura.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cbxCobertura.FormattingEnabled = true;
+            this.cbxCobertura.Items.AddRange(new object[] {
+            "Chocolate",
+            "Morango",
+            "Bosta de camelo",
+            "Leite condensado"});
+            this.cbxCobertura.Location = new System.Drawing.Point(82, 73);
+            this.cbxCobertura.Name = "cbxCobertura";
+            this.cbxCobertura.Size = new System.Drawing.Size(121, 21);
+            this.cbxCobertura.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(209, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 17);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Frescuras:";
+            // 
+            // cbxFrescuras
+            // 
+            this.cbxFrescuras.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cbxFrescuras.FormattingEnabled = true;
+            this.cbxFrescuras.Items.AddRange(new object[] {
+            "Tomate",
+            "Sal",
+            "Cuminho",
+            "Coloral",
+            "Cuentro",
+            "Salcinha",
+            "Granulado",
+            "Pedra"});
+            this.cbxFrescuras.Location = new System.Drawing.Point(290, 73);
+            this.cbxFrescuras.Name = "cbxFrescuras";
+            this.cbxFrescuras.Size = new System.Drawing.Size(121, 21);
+            this.cbxFrescuras.TabIndex = 7;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox3.Controls.Add(this.txtDinheiro);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.txtTotal);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Location = new System.Drawing.Point(91, 277);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(424, 68);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Valor:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(6, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 16);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Total:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtTotal.Location = new System.Drawing.Point(54, 32);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(209, 33);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 16);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Dinheiro:";
+            // 
+            // txtDinheiro
+            // 
+            this.txtDinheiro.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtDinheiro.Location = new System.Drawing.Point(276, 32);
+            this.txtDinheiro.Name = "txtDinheiro";
+            this.txtDinheiro.Size = new System.Drawing.Size(100, 20);
+            this.txtDinheiro.TabIndex = 7;
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnPagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagar.Location = new System.Drawing.Point(100, 361);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(103, 40);
+            this.btnPagar.TabIndex = 8;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.Control;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(399, 361);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(103, 40);
+            this.btnSair.TabIndex = 9;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(601, 413);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxTipo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbxFlocos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbxCreme;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxFrescuras;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbxCobertura;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtDinheiro;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnPagar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ projeto_sorveteria/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_sorveteria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int dinheiro;
+        public static int quantidade;
+        
+        private void cbxTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxTipo.Text == "Morango")
+            {
+                double.Parse(txtTotal.Text += 1.50);
+            }
+            else if (cbxTipo.Text == "Chocolate")
+            {
+                double.Parse(txtTotal.Text += 1.50);
+            }
+            else if (cbxTipo.Text == "Maçã")
+            {
+                double.Parse(txtTotal.Text += 2.00);
+            }
+            else if (cbxTipo.Text == "Cenoura")
+            {
+                double.Parse(txtTotal.Text += 3.50);
+            }
+            else if (cbxTipo.Text == "Sabão")
+            {
+                double.Parse(txtTotal.Text += 5.00);
+            }
+            else if (cbxTipo.Text == "Pneu usado")
+            {
+                double.Parse(txtTotal.Text += 7.50);
+            }
+            else if (cbxTipo.Text == "Maracujá")
+            {
+                double.Parse(txtTotal.Text += 2.50);
+            }
+            else if (cbxTipo.Text == "Flocos")
+            {
+                double.Parse(txtTotal.Text += 2.50);
+            }
+            else if (cbxTipo.Text == "Aquele bagui azul lá")
+            {
+                double.Parse(txtTotal.Text += 3.50);
+            }
+        }
+        
+    }
+}
